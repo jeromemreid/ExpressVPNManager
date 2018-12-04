a087c30e236bfe6392bbd38487f604db8a45df47
Jerome Reid <jeromemreid@me.com>
2018-12-04 00:52:23 +0000

    "adding leaf"

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -11,6 +11,14 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
+    
+    router.get("status") { req -> String in
+        
+        let expressVPN = SocketPort (remoteWithTCPPort: 4201, host: "192.168.1.222")
+        expressVPN?.send(before: Date(timeIntervalSinceNow: 1000), components: ["expressvpn status"],  from: nil, reserved: 100)
+        let status = "Connected"
+        return "status is \(status)"
+    }
 
     // Example of configuring a controller
     let todoController = TodoController()
